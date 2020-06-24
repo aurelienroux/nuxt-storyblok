7779c6eea9a6130d4aa0fd85890b7992771fc485
Aurélien Roux <aurelien.roux@aerial.ai>
2020-06-24 15:37:48 -0400

    basic component and display

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [],
+  plugins: ['~/plugins/components'],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
@@ -54,7 +54,12 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: [
+    [
+      "storyblok-nuxt",
+      { accessToken: "RqrHjFPvqGc5wibVovgTXwtt", cacheProvider: "memory" }
+    ]
+  ],
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
